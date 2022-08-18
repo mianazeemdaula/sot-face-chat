0dbda3b17ed0abfe20b1082bc237872dae7118bb
mianazeemdaula <mianazeemdaula@gmail.com>
2022-08-18 17:46:20 +0500

    18-08-2022

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:face_chat/core/app_navigator.dart';
 import 'package:face_chat/views/auth/login_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -20,6 +21,54 @@ class HomeView extends StatelessWidget {
             icon: Icon(Icons.logout),
           )
         ],
+      ),
+      body: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        future: FirebaseFirestore.instance.collection('posts').get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Material(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            snapshot.data!.docs[index].data()['body'],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                snapshot.data!.docs[index]
+                                    .data()['likes']
+                                    .toString(),
+                              ),
+                              Text(
+                                snapshot.data!.docs[index]
+                                    .data()['comments']
+                                    .toString(),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
